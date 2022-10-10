20828a4ddbf98eadd9f9f0761a4d5a7759f9a1c3
Raphael Ribeiro Lino <raphar.lino@gmail.com>
2022-10-09 23:57:10 -0300

    First commit

---
+++ src/test/java/com/br/planejamento/financeiro/PlanejamentoFinanceiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.planejamento.financeiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanejamentoFinanceiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
