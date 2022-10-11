a3e7bba2bc10c67ccf0f20f439fb6d5aa8f7aa77
Raphael Ribeiro Lino <raphar.lino@gmail.com>
2022-10-10 22:03:58 -0300

    Adicionando exceptions

---
+++ src/main/java/com/br/planejamento/financeiro/exceptions/BadRequestBussinessException.java
@@ -0,0 +1,11 @@
+package com.br.planejamento.financeiro.exceptions;
+
+public class BadRequestBussinessException extends BusinessException {
+
+	private static final long serialVersionUID = 1L;
+
+	public BadRequestBussinessException(String message) {
+		super(message);
+	}
+
+}
---
+++ src/main/java/com/br/planejamento/financeiro/exceptions/NotFoundBussinessException.java
@@ -0,0 +1,11 @@
+package com.br.planejamento.financeiro.exceptions;
+
+public class NotFoundBussinessException extends BusinessException {
+
+	private static final long serialVersionUID = 1L;
+
+	public NotFoundBussinessException(String message) {
+		super(message);
+	}
+
+}
