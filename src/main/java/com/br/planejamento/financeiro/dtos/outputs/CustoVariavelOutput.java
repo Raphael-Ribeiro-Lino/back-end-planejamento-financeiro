6e42041b27e56c4b8538129ab296875336862cd9
Raphael Ribeiro Lino <raphar.lino@gmail.com>
2022-10-12 22:31:53 -0300

    Adicionando output custo variavel

---
+++ src/main/java/com/br/planejamento/financeiro/dtos/outputs/CustoVariavelOutput.java
@@ -0,0 +1,15 @@
+package com.br.planejamento.financeiro.dtos.outputs;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CustoVariavelOutput {
+	
+	private Long id;
+	
+	private String nome;
+	
+	private Float valor;
+}
