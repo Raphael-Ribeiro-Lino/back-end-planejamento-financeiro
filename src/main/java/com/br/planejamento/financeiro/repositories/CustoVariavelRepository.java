d2abf47433bf822478472fa70384e90f7ef8c1d5
Raphael Ribeiro Lino <raphar.lino@gmail.com>
2022-10-12 22:25:08 -0300

    Adicionando repository custo variavel

---
+++ src/main/java/com/br/planejamento/financeiro/repositories/CustoVariavelRepository.java
@@ -0,0 +1,9 @@
+package com.br.planejamento.financeiro.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.br.planejamento.financeiro.entities.CustoVariavelEntity;
+
+public interface CustoVariavelRepository extends JpaRepository<CustoVariavelEntity, Long>{
+
+}
