0e3677bf0d5e80a255fd2baabf202342990e1826
Raphael Ribeiro Lino <raphar.lino@gmail.com>
2022-10-12 21:34:26 -0300

    Adicionando convert custo fixo

---
+++ src/main/java/com/br/planejamento/financeiro/converts/CustoFixoConvert.java
@@ -0,0 +1,13 @@
+package com.br.planejamento.financeiro.converts;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustoFixoConvert {
+
+	@Autowired
+	private ModelMapper modelMapper;
+	
+}
