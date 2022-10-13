9b851ed3609fb8c2858145fc5f4e452428aaddef
Raphael Ribeiro Lino <raphar.lino@gmail.com>
2022-10-12 21:36:51 -0300

    Adicionando input custo fixo

---
+++ src/main/java/com/br/planejamento/financeiro/dtos/inputs/CustoFixoInput.java
@@ -0,0 +1,21 @@
+package com.br.planejamento.financeiro.dtos.inputs;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CustoFixoInput {
+
+	@NotBlank(message = "O atributo nome é obrigatório")
+	@Length(max = 100, message = "O atributo nome pode ter no máximo 100 caracteres")
+	private String nome;
+	
+	@NotNull(message = "O atributo valor é obrigatório")
+	private Float valor;
+}
