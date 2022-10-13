c800eff2b5973ac6819e25ef1f70479e20ea512e
Raphael Ribeiro Lino <raphar.lino@gmail.com>
2022-10-12 21:31:12 -0300

    Adicionando repository custo fixo

---
+++ src/main/java/com/br/planejamento/financeiro/repositories/CustoFixoRepository.java
@@ -0,0 +1,9 @@
+package com.br.planejamento.financeiro.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.br.planejamento.financeiro.entities.CustoFixoEntity;
+
+public interface CustoFixoRepository extends JpaRepository<CustoFixoEntity, Long>{
+
+}
